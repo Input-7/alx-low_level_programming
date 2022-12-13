491432f08de8607fa2f490f7c01f7761d3227b4d
inpt-7 <xmylevin@gmail.com>
2022-12-13 05:42:32 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-int alph = 'a';
+	int alph = 'a';
 
 	while (alph <= 'z')
 	{
-putchar(alph);
+		putchar(alph);
 		alph++;
 	}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }

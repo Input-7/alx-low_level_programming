963c35604ff833d127b47ac0546a13bb0a8f789a
inpt-7 <xmylevin@gmail.com>
2022-12-13 15:27:34 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 
 	for (ten = 0; ten <= 9; ten++)
 	{
-		for (alph + 'a'; alph <= 'z'; alph++)
+		for (alph = 'a'; alph <= 'z'; alph++)
 			_putchar(alph);
 		_putchar('\n');
 	}

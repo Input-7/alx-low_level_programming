88c3449b2ad9fbb8b6325afe13dba687a0e756f1
inpt-7 <xmylevin@gmail.com>
2022-12-22 03:55:04 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,27 @@
 #include "main.h"
-/**
- * _strncpy - check the code
- * @src: parameter to a src to copy
- * @dest: parameter for dest
- * @n: parameter for number of bytes
- * Return: Always 0.
- */
 
+/**
+ * _strncpy - function that copies a string, including the
+ * terminating null byte, using at most an inputed
+ * number of bytes
+ * If the length of the source string is less than the
+ * maximum byte number, the renainder if the destination
+ * string is filled with null bytes.
+ * Works identically to tge standard library function 'strncpy'
+ * @dest:buffer storing the string copy
+ * @src: the source string
+ * @n: maximum number of byte copied
+ * Return: returns
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
+	int i;
 
-	while (a != n)
-	{
-		dest[b] = src[a];
-		b++;
-		a++;
-		if (src[a] == '\0')
-		{
-			break;
-		}
-	}
-	while (b != n)
-		dest[b++] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }

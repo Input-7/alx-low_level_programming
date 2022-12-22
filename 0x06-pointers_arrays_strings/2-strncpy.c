946ae67e2add6a5b81b3cc9309bbee5c251f97b0
inpt-7 <xmylevin@gmail.com>
2022-12-22 03:37:46 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * _strncpy - check the code for Holberton School students.
+ * _strncpy - check the code
  * @src: parameter to a src to copy
  * @dest: parameter for dest
  * @n: parameter for number of bytes

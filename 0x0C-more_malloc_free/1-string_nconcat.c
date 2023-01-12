3b89efb05a17a9515b6eba4b64342dd50b40d9d9
inpt-7 <xmylevin@gmail.com>
2023-01-12 05:26:27 -0800

    string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - concatenates two strings
+ * @s1:stirng 1
+ * @s2:string 2
+ * @n: n bytesofstring 2
+ * Return: pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+  char *ptr, *empt;
+  unsigned int i, k;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-i
-	if (arr == NULL)
-		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2];
-		co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+  empt = "";
+  if (s1 == NULL)
+    s1 = empt;
+  if (s2 == NULL)
+    s2 = empt;
+  for (i = 0; s1[i] != '\0'; i++)
+    ;
+  ptr = malloc((i + (n * sizeof(*s2) + 1) * sizeof(*ptr)));
+  if (ptr == NULL)
+    return (NULL);
+  for (i = 0; s1[i] != '\0'; i++)
+    ptr[i] = s1[i];
+  for (k = 0; s2[k] != '\0' && k < n; k++, i++)
+    ptr[i] = s2[k];
+  ptr[i] = '\0';
+  return (ptr);
 }

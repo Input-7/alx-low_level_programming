4c549701fc3cc2becb60f6067848c40e096b4af3
inpt-7 <xmylevin@gmail.com>
2023-01-12 05:31:05 -0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
 /**
- *_calloc - allocate memory for an array usingmalloc
- * @nmemb: number ofelements
- * @size: size of bytes
- * Return: pointer
+ * _calloc - print 0s into 2D grid
+ * @nmemb: array num
+ * @size: size of ea array element
+ * Return: 0
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *ptr;
+	char *arr;
+	unsigned int i;
 
-  if (nmemb == 0 || size == 0)
-    return (NULL);
-  ptr = malloc(nmemb * size);
-  if (ptr == NULL)
-    return (NULL);
-  _memset(ptr, 0, (nmemb * size));
-  return (ptr);
-}
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-/**
- *_memset - sets first n bytes of the memory area
- * @s: array to set
- * @b: what to set it to
- * @n: first n bytes
- * Return: s
- */
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
 
-char *_memset(char *s, char b, unsigned int n)
-{
-  unsigned int i;
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
 
-  for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-  return (s);
+	return (arr);
 }
